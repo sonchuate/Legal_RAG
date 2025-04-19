814b2bd589cf62b70ef8c3c6b80af031c8b4ef31
Tran Cao Son 20215472 <caosonth2003@gmail.com>
2025-04-19 09:47:45 +0700

    update conveter

--- rag/utils/converter.py
+++ rag/utils/converter.py
@@ -4,19 +4,31 @@ class Converter:
     def __init__(self):
         pass
 
-    def response2graph(self, response:str) -> Graph:
+    def response2graph(self, response: str) -> Graph:
         response = response.split('<|COMPLETE|>')[0]
         name2node = {}
         edges = []
 
         for line in response.split('##'):
-            if line.startswith('("entity"'):
-                _, node_name, node_type, node_description, _ = line.split('<|>')
-                node = Node(node_type.upper(), {"name": node_name, "node_description": node_description})
-                name2node[node_name] = node
-        
-            if line.startswith('("relationship'):
-                _, node_name_1, node_name_2, rel_type, ao = line[1:-1]
-                edges.append(Edge(name2node[node_name_1], name2node[node_name_2], rel_type + "|" + ao))
+            line = line.strip()
+            if not line:
+                continue
 
-    return Graph(name2node.values(), edges)
+            if line.startswith('("entity'):
+                parts = line.strip('()').split('<|>')
+                _, node_name, node_type, node_description, _ = parts
+                name2node[node_name] = Node(
+                    node_type.upper(),
+                    {"name": node_name, "node_description": node_description}
+                    )
+
+            elif line.startswith('("relationship'):
+                parts = line.strip('()').split('<|>')
+                _, node_name_1, node_name_2, rel_type, ao = parts
+                edges.append(Edge(
+                    name2node[node_name_1],
+                    name2node[node_name_2],
+                    rel_type + "|" + ao
+                ))
+
+        return Graph(list(name2node.values()), edges)
